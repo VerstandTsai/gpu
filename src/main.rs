c40b2075d1bb722f3f42eb81b83136d49d2c1657
Verstand Tsai <hackertsaigenz@gmail.com>
2026-07-20 17:39:22 +0800

    tex_coords

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,18 @@ mod vertex;
 use crate::app::App;
 
 define_vertex! { TriangleVertex;
-    position: [f32; 3]
+    position: [f32; 3],
+    tex_coords: [f32; 2]
 }
 
 fn main() {
     let vertices = vec![
-        TriangleVertex { position: [-1.,  1., 0.] },
-        TriangleVertex { position: [ 1.,  1., 0.] },
-        TriangleVertex { position: [ 1., -1., 0.] },
-        TriangleVertex { position: [ 1., -1., 0.] },
-        TriangleVertex { position: [-1., -1., 0.] },
-        TriangleVertex { position: [-1.,  1., 0.] },
+        TriangleVertex { position: [-1.,  1., 0.], tex_coords: [0., 0.] },
+        TriangleVertex { position: [ 1.,  1., 0.], tex_coords: [1., 0.] },
+        TriangleVertex { position: [ 1., -1., 0.], tex_coords: [1., 1.] },
+        TriangleVertex { position: [ 1., -1., 0.], tex_coords: [1., 1.] },
+        TriangleVertex { position: [-1., -1., 0.], tex_coords: [0., 1.] },
+        TriangleVertex { position: [-1.,  1., 0.], tex_coords: [0., 0.] },
     ];
     let app = App::new(include_str!("shader.wgsl"), vertices);
     app.run().expect("Cannot run the app");
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,26 +1,27 @@
 struct VertexInput {
     @location(0) position: vec3<f32>,
+    @location(1) tex_coords: vec2<f32>,
 };
 
 struct VertexOutput {
     @builtin(position) position: vec4<f32>,
-    @location(0) pos2d: vec2<f32>,
+    @location(0) tex_coords: vec2<f32>,
 };
 
 @vertex
 fn vs_main(model: VertexInput) -> VertexOutput {
     var out: VertexOutput;
     out.position = vec4<f32>(model.position, 1.0);
-    out.pos2d = model.position.xy;
+    out.tex_coords = model.tex_coords.xy;
     return out;
 }
 
-fn get_color(pos2d: vec2<f32>) -> f32 {
-    return sin(64 * pos2d.x) * cos(64 * pos2d.y);
+fn get_color(tex_coords: vec2<f32>) -> f32 {
+    return sin(64 * tex_coords.x) * cos(64 * tex_coords.y) / 2 + 0.5;
 }
 
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-    return vec4<f32>(vec3<f32>(pow(get_color(in.pos2d) / 2 + 0.5, 2.2)), 1.0);
+    return vec4<f32>(vec3<f32>(pow(get_color(in.tex_coords), 2.2)), 1.0);
 }
 
